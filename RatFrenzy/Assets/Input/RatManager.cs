4b556da42d55a319180d8bdc01212e12f9b54e00
Gabriel <gabriel_tran@hotmail.com>
2020-01-03 21:02:54 -0500

    Using playerinput properly for local coop

--- RatFrenzy/Assets/Input/RatManager.cs
+++ RatFrenzy/Assets/Input/RatManager.cs
@@ -2,55 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class RatManager : MonoBehaviour
 {
-    [SerializeField]
-    public RatControls controls;
+
     public Vector2 move { get; private set; }
     public bool jump { get; private set; }
     public bool interact { get; private set; }
     public RatInputEvent onJump = new RatInputEvent(), onInteract = new RatInputEvent();
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnMove(InputValue value)
     {
-
-        controls.DefaultRat.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
-        controls.DefaultRat.Move.performed += ctx => PlayerMoving();
-        controls.DefaultRat.Move.canceled += ctx => move = Vector2.zero;
-        controls.DefaultRat.Jump.performed += ctx => Jump();
-
-        // Ça c'est facultatif
-        controls.DefaultRat.Jump.performed += ctx => jump = true;
-        controls.DefaultRat.Jump.canceled += ctx => jump = false;
-        controls.DefaultRat.Interact.performed += ctx => Interact();
-        controls.DefaultRat.Interact.performed += ctx => interact = true;
-        controls.DefaultRat.Interact.canceled += ctx => interact = false;
-
+        move = value.Get<Vector2>();
+        Debug.Log("Moving : " + move);
     }
-    private void PlayerMoving()
+    private void OnMoveCancelled()
     {
-        Debug.Log("Moved : " + move);
+        move = Vector2.zero;
     }
-    void Jump()
+    private void OnJump()
     {
-        Debug.Log("Jump");
+        Debug.Log("Jump!");
         onJump.Invoke();
     }
-    void Interact()
+    private void OnInteract()
     {
-        Debug.Log("Interact");
+        Debug.Log("Interact!");
         onInteract.Invoke();
     }
-    private void OnEnable()
-    {
-        if (controls == null) controls = new RatControls();
-        controls.DefaultRat.Enable();
-    }
-    private void OnDisable()
-    {
-        controls.DefaultRat.Disable();
-    }
+
 }
 
 [System.Serializable]
