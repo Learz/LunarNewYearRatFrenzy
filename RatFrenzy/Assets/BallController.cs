ec1e36ceb84cc91b0e2e5ad257dc2fc837ae3c4f
Learz <marco456789123@hotmail.com>
2020-01-04 23:17:27 -0500

    Added jump animation
    First implementation in mecanim

--- RatFrenzy/Assets/BallController.cs
+++ RatFrenzy/Assets/BallController.cs
@@ -15,6 +15,8 @@ public class BallController : MonoBehaviour
     private Animator rAnim;
     private Vector3 vel;
     private float realSpeed, realJumpHeight, ratYOffset;
+
+    private const float defaultDrag = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +28,7 @@ public class BallController : MonoBehaviour
         if (!isSumo)
         {
             ball.SetActive(false);
-            rb.drag = 20;
+            rb.drag = defaultDrag;
             realSpeed = speed * 10;
             realJumpHeight = jumpHeight;
             ratYOffset = 0.5f;
@@ -40,18 +42,25 @@ public class BallController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         MoveBall();
         AnimateRat();
     }
+
     public void Jump()
     {
         if (!isJumping)
         {
+            if(!isSumo)
+            {
+                rb.drag = 1;
+                realSpeed = speed;
+            }
+            
             rb.AddForce(Vector3.up * realJumpHeight);
             isJumping = true;
+            rAnim.SetBool("isJumping", isJumping);
         }
 
     }
@@ -75,6 +84,8 @@ public class BallController : MonoBehaviour
         dir.y = 0;
         rb.AddForce(dir * realSpeed);
         Debug.DrawRay(transform.position, dir, Color.green);
+
+        
     }
 
     //Animates the angle and mecanim state of the rat
@@ -92,11 +103,16 @@ public class BallController : MonoBehaviour
         rat.transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
 
         rAnim.SetFloat("velocity", rb.velocity.magnitude);
-
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         isJumping = false;
+        rAnim.SetBool("isJumping", isJumping);
+        if (!isSumo)
+        {
+            rb.drag = defaultDrag;
+            realSpeed = speed * 10;
+        }
     }
 }
