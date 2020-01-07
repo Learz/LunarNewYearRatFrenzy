5a7795f0846f5dae562937e3617240bfac8bdd52
Learz <marco456789123@hotmail.com>
2020-01-06 19:58:56 -0500

    Added area and array fields to spawner, made it a prefab
    Can spawn different objects randomly in a random position in the area

--- RatFrenzy/Assets/SpawnObject.cs
+++ RatFrenzy/Assets/SpawnObject.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SpawnObject : MonoBehaviour
 {
-    public GameObject obj;
+    public GameObject[] objects;
+    public Vector3 area = new Vector3(1,1,1);
     public float rate, increase, maxRate;
     
     private float spawnTime;
@@ -23,7 +24,21 @@ public class SpawnObject : MonoBehaviour
         if (spawnTime >= rate)
         {
             spawnTime = 0;
-            Instantiate(obj, transform.position, transform.rotation);
+            Instantiate(objects[Random.Range(0,objects.Length-1)], 
+                new Vector3
+                (
+                    transform.position.x + Random.Range(-area.x / 2, area.x / 2), 
+                    transform.position.y + Random.Range(-area.y / 2, area.y / 2), 
+                    transform.position.z + Random.Range(-area.z / 2, area.z / 2)
+                ), 
+                transform.rotation);
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow cube at the transform position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, area);
     }
 }
