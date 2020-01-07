8f520fd5fe2d2a99eb56f1c099782487595cf23a
Gabriel <gabriel_tran@hotmail.com>
2020-01-07 17:33:53 -0500

    Moved scripts to scripts folder

---
+++ RatFrenzy/Assets/Scripts/GenericWinCondition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GenericWinCondition : MonoBehaviour
+{
+    public int pointGoal;
+    public float timeLimit;
+    public bool startCountdownOnStart;
+
+    void Start()
+    {
+        if (startCountdownOnStart) StartTimeoutCountdown(timeLimit);
+    }
+
+    public virtual void EliminatePlayer(GameManager.PlayerIdentity id)
+    {
+
+    }
+    public virtual void AddMinigamePoint(GameManager.PlayerIdentity id, int points)
+    {
+
+    }
+    public virtual void StartTimeoutCountdown() => StartTimeoutCountdown(timeLimit);
+    public virtual void StartTimeoutCountdown(float limit)
+    {
+
+    }
+}
