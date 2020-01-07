f6c82656cc2401b29a9888476d922db8cd7f9beb
Gabriel <gabriel_tran@hotmail.com>
2020-01-06 23:16:36 -0500

    TreadmillPrototype

---
+++ RatFrenzy/Assets/Treadmill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treadmill : MonoBehaviour
+{
+    public float speed, textureDampener = 500;
+    public Material mat;
+    private float offset = 0;
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+        //collision.rigidbody.AddForce(-transform.forward * speed * Time.deltaTime);
+        //collision.transform.Translate(-transform.forward * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        GetComponent<Rigidbody>().MovePosition(transform.position - transform.forward * speed * Time.deltaTime);
+        speed = Time.time / 10;
+        //transform.Translate(-transform.forward * speed * Time.deltaTime);
+        /*offset = Time.time * speed / textureDampener;
+        mat.SetTextureOffset("_DetailMap", new Vector2(0, offset));*/
+    }
+}
