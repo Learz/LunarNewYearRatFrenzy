3cd4786b329b3a904c2537561674bd8d98bd2591
Learz <marco456789123@hotmail.com>
2020-01-09 18:07:23 -0500

    Added a acceleration modifier based on Time
    Tweaked the settings in towerclimb and twisted system for an enjoyable experience

--- RatFrenzy/Assets/Scripts/Moving.cs
+++ RatFrenzy/Assets/Scripts/Moving.cs
@@ -5,25 +5,27 @@ using DG.Tweening;
 
 public class Moving : MonoBehaviour
 {
-    public float speed;
+    public float speed, acceleration, maxSpeed;
     public Vector3 translation, rotation;
-
     public bool dies;
     public float lifeSpan;
+
+    private float realspeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(translation * speed * Time.deltaTime);
-        transform.Rotate(rotation * speed * Time.deltaTime);
+        transform.Translate(translation * realspeed * Time.deltaTime);
+        transform.Rotate(rotation * realspeed * Time.deltaTime);
         lifeSpan -= Time.deltaTime;
         if (dies && lifeSpan <= 0) Destroy(this.gameObject);
+        realspeed = Mathf.Clamp(speed + Time.timeSinceLevelLoad * acceleration, 0, maxSpeed);
     }
 
-    
+
 }
