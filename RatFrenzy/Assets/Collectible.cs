7ee5bd3e8aaf3a459848e86ee88b57eff11b8e8b
Gabriel <gabriel_tran@hotmail.com>
2020-01-09 15:36:16 -0500

    Added Lab CleanUp prototype

---
+++ RatFrenzy/Assets/Collectible.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GenericController ctrl = other.GetComponent<GenericController>();
+        if (ctrl == null) return;
+        ctrl.AddPoint();
+        Destroy(this.gameObject);
+    }
+}
---
+++ RatFrenzy/Assets/SpawnOnCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnCollision : MonoBehaviour
+{
+    public GameObject prefab;
+    public ParticleSystem PSystem;
+    private List<ParticleCollisionEvent> CollisionEvents;
+    private void Start()
+    {
+        CollisionEvents = new List<ParticleCollisionEvent>();
+    }
+    public void OnParticleCollision(GameObject other)
+    {
+        int collCount = PSystem.GetSafeCollisionEventSize();
+
+        int eventCount = PSystem.GetCollisionEvents(other, CollisionEvents);
+
+        foreach (ParticleCollisionEvent collision in CollisionEvents)
+        {
+            Vector3 pos = new Vector3(collision.intersection.x, collision.intersection.y - PSystem.main.startSize.constant / 2, collision.intersection.z);
+            Instantiate(prefab, pos, Quaternion.identity);
+        }
+    }
+}
