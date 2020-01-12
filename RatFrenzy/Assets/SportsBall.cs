69ca76cb05acbd15085abbf670d38869ff85accd
Gabriel <gabriel_tran@hotmail.com>
2020-01-11 20:02:08 -0500

    Started experimenting with sportsball

---
+++ RatFrenzy/Assets/SportsBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SportsBall : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float contactForce, explosionRadius, upwardsForce;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.GetComponent<GenericController>() != null)
+        {
+            Debug.Log("Contact");
+            rb.AddExplosionForce(contactForce, collision.GetContact(0).point, explosionRadius, upwardsForce);
+        }
+    }
+}
