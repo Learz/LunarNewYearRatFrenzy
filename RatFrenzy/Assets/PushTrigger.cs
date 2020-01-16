0a55af1a545bb04a45fc5671ada3140317a8d838
Gabriel <gabriel_tran@hotmail.com>
2020-01-15 22:27:20 -0500

    Added dynamic camera to sumoarena.unity, added numbers to identify goals in sportsball.unity, brightened stars in skybox hdr for winscreen.unity

---
+++ RatFrenzy/Assets/PushTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushTrigger : MonoBehaviour
+{
+    public Vector3 force;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb != null) rb.AddForce(transform.TransformPoint(force) * Time.deltaTime);
+    }
+}
